5d41b331fd39145e1c6cb86fba6e2fa6ab5022f9
unknown <gabi.palmasantos@hotmail.com>
2021-03-07 19:13:16 -0300

    commit inicial

---
+++ style/MainStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet, View } from 'react-native';
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#def',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderColor: "pink",
+      borderWidth: 5,
+      borderStyle: "solid"
+    },
+  });
+
+  export default styles
